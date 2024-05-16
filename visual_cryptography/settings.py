fbdf760f9625c451905a52bfd471b160c55dddf6
Thanh Tran Tan <thanhtt@techhaus.vn>
2024-05-17 00:32:44 +0700

    first upload

--- visual_cryptography/settings.py
+++ visual_cryptography/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'applications',
-    'visual_cryptography'
+    'applications'
 ]
 
 MIDDLEWARE = [
